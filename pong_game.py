5f2e2b063142873314760979419e1b322c812f5f
jaskaran <jaskaran.sarkaria@googlemail.com>
2018-03-24 16:43:12 +0000

    finish basic game

--- pong_game.py
+++ pong_game.py
@@ -1,10 +1,45 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
+from kivy.clock import Clock
 from kivy.vector import Vector
 
+
 class PongGame(Widget):
-    pass
+    ball = ObjectProperty(None)
+    player1 = ObjectProperty(None)
+    player2 = ObjectProperty(None)
+
+
+    def on_touch_move(self, touch):
+        if touch.x < self.width/ 3:
+            self.player1.center_y = touch.y
+        if touch.x > self.width - self.width/3:
+            self.player2.center_y = touch.y
+
+    def serve_ball(self, vel=(4, 0)):
+        self.ball.center = self.center
+        self.ball.velocity = vel
+
+    def update(self, dt):
+        self.ball.move()# call ball.move and other stuff
+
+        #bounce off paddles
+        self.player1.bounce_ball(self.ball)
+        self.player2.bounce_ball(self.ball)
+
+        # bounce off top and bottom
+        if self.ball.y < 0 or self.ball.top > self.height:
+            self.ball.velocity_y *= -1
+
+        # bounce off left and right
+        if self.ball.x < self.x:
+            self.player2.score += 1
+            self.serve_ball(vel=(4, 0))
+        if self.ball.x > self.width:
+            self.player1.score += 1
+            self.serve_ball(vel=(-4, 0))
+        pass
 
 class PongBall(Widget):
 
@@ -22,10 +57,24 @@ class PongBall(Widget):
     def move(self):
         self.pos = Vector(*self.velocity) + self.pos
 
+class PongPaddle(Widget):
+    score = NumericProperty(0)
+
+    def bounce_ball(self, ball): # this algorithm has strange behaviour if the ball hits the
+                            # paddle from the side or the bottom
+        if self.collide_widget(ball):
+            vx, vy = ball.velocity
+            offset = (ball.center_y - self.center_y)/ (self.height / 2)
+            bounced = Vector(-1 * vx, vy)
+            vel = 1.1 * bounced
+            ball.velocity = vel.x, vel.y + offset
 
 class PongApp(App):
     def build(self):
-        return PongGame()
+        game = PongGame()
+        game.serve_ball()
+        Clock.schedule_interval(game.update, 1.0/60.0) # updates 60 times a second
+        return game
 
 if __name__ == '__main__':
     PongApp().run()
